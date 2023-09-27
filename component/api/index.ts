9fd8562e0bfa9d488ac4c242f227588b9dd217e3
rahelerahimi <rahimirahele16@gmail.com>
2023-09-27 18:43:54 +0330

    ssr

--- component/api/index.ts
+++ component/api/index.ts
@@ -3,8 +3,11 @@
 import { MovieData } from "../type/type";
 
  export const getDataMovie = async ():Promise<{data:MovieData[]}>   => {
-
-     const data = await fetch('https://moviesapi.ir/api/v1/movies/')
+     const data = await fetch('https://moviesapi.ir/api/v1/movies/' , {
+          headers: {
+            'Cache-Control': 'no-store',
+          },
+        })
      return data.json()
 }
 
@@ -18,7 +21,11 @@ export const getStaticProps= async () :Promise<{data:MovieData[]}> => {
 
  export const getDataSingle = async (id:number):Promise<MovieData>  => {
 
-     const data = await fetch(`https://moviesapi.ir/api/v1/movies/${id}`)
+     const data = await fetch(`https://moviesapi.ir/api/v1/movies/${id}`,  {
+          headers: {
+            'Cache-Control': 'no-store',
+          },
+        })
      return data.json()
 }
 
